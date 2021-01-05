f74d9c492aa047477610bd93547e000ff7ea11d3
18127003 <18127003@users.noreply.github.com>
2021-01-05 16:10:47 +0700

    Update map.js

--- script/map.js
+++ script/map.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // let side_nav = M.Sidenav.init(document.querySelectorAll(".sidenav"), {edge: "left",draggable: true,});
-    // let dropdown = M.Dropdown.init(document.querySelectorAll(".dropdown-trigger"),{ hover: true });
-    // let profile_modal = M.Modal.init(document.querySelectorAll(".modal"));
-    // var tap = M.TapTarget.init(document.querySelectorAll('.tap-target'));
-    // let t = M.Tooltip.init(document.querySelectorAll(".tooltipped"));
+    let side_nav = M.Sidenav.init(document.querySelectorAll(".sidenav"), {edge: "left",draggable: true,});
+    let dropdown = M.Dropdown.init(document.querySelectorAll(".dropdown-trigger"),{ hover: true });
+    let profile_modal = M.Modal.init(document.querySelectorAll(".modal"));
+    var tap = M.TapTarget.init(document.querySelectorAll('.tap-target'));
+    let t = M.Tooltip.init(document.querySelectorAll(".tooltipped"));
     // let edit_button = M.FloatingActionButton.init(document.querySelectorAll('.fixed-action-btn'), {direction:"right"});
     let parallax = M.Parallax.init(document.querySelectorAll(".parallax"));
 });
