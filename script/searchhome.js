5b9c3fdd6f587042dfd18d43c1e86935c7491b75
18127003 <18127003@users.noreply.github.com>
2021-01-11 22:10:20 +0700

    responsive 2

--- script/searchhome.js
+++ script/searchhome.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     let side_nav = M.Sidenav.init(document.querySelectorAll(".sidenav"), {edge: "left",draggable: true,});
     let dropdown = M.Dropdown.init(document.querySelectorAll(".dropdown-trigger"),{ hover: true });
     let profile_modal = M.Modal.init(document.querySelectorAll(".modal"));
-    let instance = M.Tabs.init(document.querySelector(".tabs"));
+    let instance = M.Tabs.init(document.querySelectorAll(".tabs"));
     let t = M.Tooltip.init(document.querySelectorAll(".tooltipped"));
-    let collap = M.Collapsible.init(document.querySelector(".collapsible"));
+    let collap = M.Collapsible.init(document.querySelectorAll(".collapsible"));
   });
   
